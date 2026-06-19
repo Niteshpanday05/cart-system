079e0e4c1572289ae9851648ab424dee34f21361
Niteshpanday05 <eagleseye.np@gmail.com>
2026-06-20 00:26:42 +0545

    problem in api/cart

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # JWT AUTH
+      
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
+
     
     # API routes
     
--- frontend/app/cart/page.js
+++ frontend/app/cart/page.js
@@ -8,20 +8,28 @@ export default function Cart() {
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    getCart(token).then(setCart);
+
+    getCart(token).then((data) => {
+      console.log("Cart API:", data);
+      setCart(Array.isArray(data) ? data : []);
+    });
   }, []);
 
   return (
     <div>
       <h1>Cart</h1>
 
-      {cart.map((item) => (
-        <div key={item.id}>
-          <p>{item.product}</p>
-          <p>{item.price}</p>
-          <p>{item.quantity}</p>
-        </div>
-      ))}
+      {cart.length === 0 ? (
+        <p>No items in cart</p>
+      ) : (
+        cart.map((item, index) => (
+          <div key={index}>
+            <p>{item.product}</p>
+            <p>{item.price}</p>
+            <p>{item.quantity}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
