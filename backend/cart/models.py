0cef15fbcffd5d80395b099bcf03a2f9cadefe97
Niteshpanday05 <eagleseye.np@gmail.com>
2026-06-19 19:48:10 +0545

    updated frontend setup

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
 
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
